7e458194d942ba7abbd12300fb94279416bff403
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 17:51:39 -0600

    click output to page

--- numpad.js
+++ numpad.js
@@ -1,9 +1,10 @@
 console.log("numpad loaded");
 
-document.addEventListener('click', () => {
-    console.log("click");
-})
+const output = document.querySelector("#output-text");
 
-const output = document.querySelector("#output-text")
+document.addEventListener("click", () => {
+  console.log("click");
+  output.innerText = "click";
+});
 
 output.innerText = "yeah!";
