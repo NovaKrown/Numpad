bd79471e46d4814cfbb972966d770c19c925180f
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 14:15:31 -0600

    connect javascript

--- numpad.js
+++ numpad.js
@@ -3,3 +3,7 @@ console.log("numpad loaded");
 document.addEventListener('click', () => {
     console.log("click");
 })
+
+const output = document.querySelector(#output-text)
+
+output.textContent = "yeah";
