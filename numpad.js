241028dc048efec536510d279eb23e293c2ce27b
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 18:29:32 -0600

    unevent

--- numpad.js
+++ numpad.js
@@ -2,9 +2,13 @@ console.log("numpad loaded");
 
 const output = document.querySelector("#output-text");
 
-document.addEventListener("click", () => {
+document.addEventListener("pointerdown", () => {
   console.log("click");
   output.innerText = "click";
+});
+
+document.addEventListener("pointerup", () => {
+  output.innerText = "boop";
 });
 
 output.innerText = "yeah!";
