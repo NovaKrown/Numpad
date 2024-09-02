b9419a74d8b8cd2ee72dcbd58165f7a77de11768
NickKumar <Timerbob@gmail.com>
2024-09-02 07:11:02 -0600

    update movement from input and styling issues

--- numpad.js
+++ numpad.js
@@ -1,19 +1,45 @@
 console.log("numpad loaded");
 
 const output = document.querySelector("#output-text");
-
 const box = document.querySelector(".box");
+let pointerEvent = false;
+const pointerLocation = {
+  x: 0,
+  y: 0,
+};
 
+let width = window.innerWidth / 100;
+let height = window.innerHeight / 100;
+let boxDimensions = width < height ? width : height;
 
-document.addEventListener("pointerdown", (event) => {
+box.style.width = `${boxDimensions * 10}px`;
+box.style.height = `${boxDimensions * 10}px`;
+
+document.addEventListener("pointerdown", () => {
   console.log("click");
-  output.innerText = `${event.clientX}, ${event.clientY}`;
-  box.style.top = `${event.clientY}`;
-  box.style.left = `${event.clientX}`;
+  pointerEvent = true;
+});
+
+document.addEventListener("pointermove", (event) => {
+  pointerLocation.x = event.clientX;
+  pointerLocation.y = event.clientY;
 });
 
 document.addEventListener("pointerup", () => {
-  output.innerText = "boo";
+  pointerEvent = false;
 });
 
 output.innerText = "ready!";
+
+const loop = () => {
+  // console.log("loooop");
+  if (pointerEvent) {
+    output.innerText = `x: ${pointerLocation.x}, y: ${pointerLocation.y}`;
+    box.style.backgroundColor = "cornflowerblue";
+    box.style.top = `${pointerLocation.y}px`;
+    box.style.left = `${pointerLocation.x}px`;
+  }
+  requestAnimationFrame(loop);
+};
+
+loop();
