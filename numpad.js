a2253e0c4eb2474cf64706832b0315bd3a801308
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 18:19:41 -0600

    version mistake

--- numpad.js
+++ numpad.js
@@ -1,5 +1,9 @@
 console.log("numpad loaded");
 
+document.addEventListener('click', () => {
+    console.log("click");
+})
+
 const output = document.querySelector("#output-text");
 
 document.addEventListener("click", () => {
