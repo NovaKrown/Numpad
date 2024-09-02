81b8b812aaca559d6f474d1deca0ff1542203ac3
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 18:21:24 -0600

    remove redundant code

--- numpad.js
+++ numpad.js
@@ -1,9 +1,5 @@
 console.log("numpad loaded");
 
-document.addEventListener('click', () => {
-    console.log("click");
-})
-
 const output = document.querySelector("#output-text");
 
 document.addEventListener("click", () => {
