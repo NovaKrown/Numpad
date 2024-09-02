79da58e2d9b1d2cb5d5122bb2cd14d3257405202
NovaKrown <101138898+NovaKrown@users.noreply.github.com>
2024-09-01 19:50:34 -0600

    clientY

--- numpad.js
+++ numpad.js
@@ -7,7 +7,7 @@ const box = document.querySelector(".box");
 
 document.addEventListener("pointerdown", (event) => {
   console.log("click");
-  output.innerText = event.clientX;
+  output.innerText = event.clientX, event.clientY;
 });
 
 document.addEventListener("pointerup", () => {
